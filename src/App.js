8af7a92c376a3d6afa943505d2e962a1d5d5db35
Edgar Manukyan <edgar.manukyan@roche.com>
2020-11-03 19:38:23 -0500

    add src and other folders

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
+                <p>v 1.0</p>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
